626f94d8f031ea457389b6ca97f13bb3539ea714
chernenkoeg_inno <chernenko_e_g@mail.ru>
2025-10-19 14:57:49 +0300

    change application logic on displaying input length (version 1)

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main (String[] args) {
+      String text = new Scanner(System.in).nextLine();
+      System.out.println("Длина текста: " + text.length());
+  }
+}
