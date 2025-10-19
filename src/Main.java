b0259133a9add9a616a9b9ae0b9307571a22617d
chernenkoeg_inno <chernenko_e_g@mail.ru>
2025-10-19 15:27:38 +0300

    adding application operation with two numbers

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main (String[] args) {
-      String text = new Scanner(System.in).nextLine();
-      System.out.println("Длина текста: " + text.length());
-  }
+    public static void main (String[] args) {
+        // Ввод первого значения
+        System.out.println ("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        // Ввод значения значения
+        System.out.println ("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        // Вычисление суммы введенных чисел
+        int addition = number1 + number2;
+        System.out.println ("Сумма введенных чисел равна:" + addition);
+        // Вычисление разности введенных чисел
+        int subtraction = number1 - number2;
+        System.out.println ("Разность введенных чисел равна:" + subtraction);
+        // Вычисление произведения введенных чисел
+        int multiplication = number1 * number2;
+        System.out.println ("Произведение введенных чисел равно:" + multiplication);
+        // Вычисление частного введенных чисел
+        double division = (double)number1 / number2;
+        System.out.println ("Частное введенных чисел равно:" + division);
+
+    }
 }
